a196e5e3b33e5eaec79f5c1d873ffd213c6f36d3
basiibas <bas.riemprasert@gmail.com>
2016-11-24 17:56:03 +0100

    added javacript

---
+++ QueueDrop/js/javascript.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  		var menuBox = document.getElementById('menu-display');    
+  			if(menuBox.style.display == "block") {
+    			menuBox.style.display = "none";
+  			}
+  	else { 
+    menuBox.style.display = "block";
+  	}
+}
